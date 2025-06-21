6af2cec175dadb4fc1e936563d0f992e0352c21d
LuisGutWo <agutierrezwong@gmail.com>
2025-06-21 03:01:30 -0400

    fix wp button: adjust top and right messueres

--- src/pages/dark/page-home/index.jsx
+++ src/pages/dark/page-home/index.jsx
@@ -17,44 +17,51 @@ import Footer from "@/components/Main/Footer";
 
 function HomeDark() {
   useEffect(() => {
-    const body = document?.body;
-    if (body) {
-      body.classList.add("sub-bg");
+    if (typeof document !== "undefined" && document.body !== null) {
+      document.body.classList.add("sub-bg");
+
       return () => {
-        if (body) {
-          body.classList.remove("sub-bg");
+        if (typeof document !== "undefined" && document.body !== null) {
+          document.body.classList.remove("sub-bg");
         }
       };
     }
   }, []);
 
-  return (
-    <>
-      <Head>
-        <title>Elephant Group</title>
-        <meta name="description" content="Elephant Group web site" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+  try {
+    return (
+      <>
+        <Head>
+          <title>Elephant Group</title>
+          <meta name="description" content="Elephant Group web site" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
 
-      <Loader />
-      <Navbar mainBg />
-      <main className="main-bg position-re">
-        <Header />
-        <Marq />
-        <Intro />
-        <Services />
-        <Portfolio />
-        <Clients />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  );
+        <Loader />
+        <Navbar mainBg />
+        <main className="main-bg position-re">
+          <Header />
+          <Marq />
+          <Intro />
+          <Services />
+          <Portfolio />
+          <Clients />
+          <Contact />
+        </main>
+        <Footer />
+      </>
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Rendering error in HomePageDark:", error);
+      return <div>Error: {error.message}</div>;
+    } else {
+      throw new Error(
+        `Uncaught error in HomePageDark component: ${error.toString()}`
+      );
+    }
+  }
 }
-
-HomeDark.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 HomeDark.getLayout = (page) => <Layout>{page}</Layout>;
 
