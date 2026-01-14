455d632319cc758437ae7b5235dc50e730e72ab8
LuisGutWo <agutierrezwong@gmail.com>
2026-01-14 04:18:27 -0300

    feat: actualizar configuración de PostCSS y eliminar archivos CSS de plugins innecesarios

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
+  plugins: [
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss").default({
+      content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/**/*.html"],
+      safelist: {
+        standard: [
+          /^active/,
+          /^show/,
+          /^is-/,
+          /^navbar/,
+          /^dropdown/,
+          /^container/,
+          /^row/,
+          /^col/,
+        ],
+        deep: [],
+        greedy: [],
+      },
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/.]+/g) || [],
+    }),
+  ],
 };
