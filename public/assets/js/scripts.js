25303680207d7e3a7d91c8b910cf0ea1a59f34c7
lagmedia <agutierrezwong@gmail.com>
2026-06-24 02:30:20 -0400

    first new branch commit

--- public/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Ensure WOW.js is loaded before using WOW
-if (typeof window !== "undefined" && typeof WOW !== "undefined")
-  new WOW({
-    animateClass: "animated",
-    offset: 100,
-  }).init();
